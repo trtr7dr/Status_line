561394a84c6c7d1521c9c530c9fc79a9da60fda0
Glo <trtr7dr@users.noreply.github.com>
2018-07-18 12:06:07 +0300

    Update statusLine.js

--- statusLine.js
+++ statusLine.js
@@ -1,17 +1,28 @@
 
 function Status_line(id, h, back, tops) {
-    this.id = (id != undefined) ? id : 'status-line';
-    this.h = (h != undefined) ? h : '5px';
-    this.back = (back != undefined) ? back : '#E91E63';
-    this.tops = (tops != undefined) ? tops : '0px';
+    this.id = typeof id !== 'undefined' ? id : 'status-line';
+    this.h = typeof h !== 'undefined' ? h : '5px';
+    this.back = typeof back !== 'undefined' ? back : '#E91E63';
+    this.tops = typeof tops !== 'undefined' ? tops : '0px';
 
     window.onload=function(){
         var d=document.createElement('div');
         d.innerHTML = '<span></span>';
         d.id = this.id;
         document.body.appendChild(d);
-        jQuery("#" + this.id ).css('position','fixed').css('z-index', '1').css('width', '100%').css('left', '0').css('top', this.tops);
-        jQuery("#" + this.id + " span").css('width', '0%').css('display', 'block').css('height', this.h).css('background', this.back);
+        jQuery("#" + this.id ).css({
+            'position' : 'fixed',
+            'z-index' : 1,
+            'width' : '100%',
+            'left' : 0,
+            'top' : this.tops
+        });
+        jQuery("#" + this.id + " span" ).css({
+            'display' : 'block',
+            'width' : '0%',
+            'height' : this.h,
+            'background' : this.back
+        });
     }
 
     window.onscroll=function(){
